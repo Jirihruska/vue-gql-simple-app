0b61839a722432c19285564767f10713b0f7265b
jirihruska <jiri.hruska@rascasone.com>
2022-10-28 21:34:38 +0200

    post

--- src/services/api.js
+++ src/services/api.js
@@ -10,7 +10,7 @@ export const api = async (graphqlQuery) => {
       headers: {
         "Access-Control-Allow-Headers":
           "origin, x-requested-with, content-type",
-        "Access-Control-Allow-Origin": `${process.env.VUE_APP_BACKEND_URL}`,
+        "Access-Control-Allow-Origin": "*",
         "Content-Type": "application/json",
         "Access-Control-Allow-Methods": "GET,POST,OPTIONS",
         "Access-Control-Allow-Credentials": true,
