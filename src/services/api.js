079252f372483b41d9afe0acec8923c6c060a16b
jirihruska <jiri.hruska@rascasone.com>
2022-10-28 17:24:21 +0200

    no-headers

--- src/services/api.js
+++ src/services/api.js
@@ -1,19 +1,11 @@
 import axios from "axios";
 
 export const api = async (graphqlQuery) => {
-  const headers = {
-    Accept: "*/*",
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Headers": "Accept",
-    "Access-Control-Allow-Origin": "*",
-    Origin: `${process.env.VUE_APP_BACKEND_URL}`,
-  };
   let res;
   try {
     await axios({
       url: process.env.VUE_APP_BACKEND_URL,
       method: "post",
-      headers: headers,
       data: graphqlQuery,
     }).then((data) => {
       if (data.status === 200) {
